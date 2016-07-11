959cb7598f9fd40113d6b0cdc159e0e525ff132b
Chris Holcombe <xfactor973@gmail.com>
2016-07-11 12:00:56 -0700

    Remove execute field from events

--- src/events.rs
+++ src/events.rs
@@ -1,14 +1,12 @@
 
 #[derive(Debug)]
 pub struct DEVICE_TRAY_MOVED {
-    execute: String,
     pub device: String,
     pub tray_open: bool,
 }
 
 #[derive(Debug)]
 pub struct BLOCK_IMAGE_CORRUPTED {
-    execute: String,
     pub node_name: String,
     pub offset: f64,
     pub size: f64,
@@ -19,7 +17,6 @@ pub struct BLOCK_IMAGE_CORRUPTED {
 
 #[derive(Debug)]
 pub struct BLOCK_IO_ERROR {
-    execute: String,
     pub nospace: bool,
     pub action: String,
     pub device: String,
@@ -29,7 +26,6 @@ pub struct BLOCK_IO_ERROR {
 
 #[derive(Debug)]
 pub struct BLOCK_JOB_COMPLETED {
-    execute: String,
     pub error: String,
     pub device: String,
     pub len: f64,
@@ -40,7 +36,6 @@ pub struct BLOCK_JOB_COMPLETED {
 
 #[derive(Debug)]
 pub struct BLOCK_JOB_CANCELLED {
-    execute: String,
     pub device: String,
     pub len: f64,
     pub offset: f64,
@@ -50,7 +45,6 @@ pub struct BLOCK_JOB_CANCELLED {
 
 #[derive(Debug)]
 pub struct BLOCK_JOB_ERROR {
-    execute: String,
     pub action: String,
     pub device: String,
     pub operation: String,
@@ -58,7 +52,6 @@ pub struct BLOCK_JOB_ERROR {
 
 #[derive(Debug)]
 pub struct BLOCK_JOB_READY {
-    execute: String,
     pub device: String,
     pub len: f64,
     pub offset: f64,
@@ -68,7 +61,6 @@ pub struct BLOCK_JOB_READY {
 
 #[derive(Debug)]
 pub struct BLOCK_WRITE_THRESHOLD {
-    execute: String,
     pub amount_exceeded: u64,
     pub node_name: String,
     pub write_threshold: u64,
@@ -76,138 +68,124 @@ pub struct BLOCK_WRITE_THRESHOLD {
 
 #[derive(Debug)]
 pub struct SHUTDOWN {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct POWERDOWN {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct RESET {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct STOP {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct RESUME {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct SUSPEND {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct SUSPEND_DISK {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct WAKEUP {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct RTC_CHANGE {
-    execute: String,
     pub offset: f64,
 }
 
 #[derive(Debug)]
 pub struct WATCHDOG {
-    execute: String,
     pub action: String,
 }
 
 #[derive(Debug)]
 pub struct DEVICE_DELETED {
-    execute: String,
     pub device: String,
     pub path: String,
 }
 
 #[derive(Debug)]
 pub struct NIC_RX_FILTER_CHANGED {
-    execute: String,
     pub name: String,
     pub path: String,
 }
 
 #[derive(Debug)]
 pub struct VNC_CONNECTED {
-    execute: String,
     pub client: String,
     pub server: String,
 }
 
 #[derive(Debug)]
 pub struct VNC_INITIALIZED {
-    execute: String,
     pub client: String,
     pub server: String,
 }
 
 #[derive(Debug)]
 pub struct VNC_DISCONNECTED {
-    execute: String,
     pub client: String,
     pub server: String,
 }
 
 #[derive(Debug)]
 pub struct SPICE_CONNECTED {
-    execute: String,
     pub client: String,
     pub server: String,
 }
 
 #[derive(Debug)]
 pub struct SPICE_INITIALIZED {
-    execute: String,
     pub client: String,
     pub server: String,
 }
 
 #[derive(Debug)]
 pub struct SPICE_DISCONNECTED {
-    execute: String,
     pub client: String,
     pub server: String,
 }
 
 #[derive(Debug)]
 pub struct SPICE_MIGRATE_COMPLETED {
-    execute: String,
-}
+
+            }
 
 #[derive(Debug)]
 pub struct ACPI_DEVICE_OST {
-    execute: String,
     pub info: String,
 }
 
 #[derive(Debug)]
 pub struct BALLOON_CHANGE {
-    execute: String,
     pub actual: f64,
 }
 
 #[derive(Debug)]
 pub struct GUEST_PANICKED {
-    execute: String,
     pub action: String,
 }
 
 #[derive(Debug)]
 pub struct QUORUM_FAILURE {
-    execute: String,
     pub reference: String,
     pub sector_num: f64,
     pub sectors_count: f64,
@@ -215,7 +193,6 @@ pub struct QUORUM_FAILURE {
 
 #[derive(Debug)]
 pub struct QUORUM_REPORT_BAD {
-    execute: String,
     pub error: String,
     pub node_name: String,
     pub sector_num: f64,
@@ -224,14 +201,12 @@ pub struct QUORUM_REPORT_BAD {
 
 #[derive(Debug)]
 pub struct VSERPORT_CHANGE {
-    execute: String,
     pub id: String,
     pub open: bool,
 }
 
 #[derive(Debug)]
 pub struct MEM_UNPLUG_ERROR {
-    execute: String,
     pub device: String,
     pub msg: String,
 }
