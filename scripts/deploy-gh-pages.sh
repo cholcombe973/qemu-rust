accd5b2a5837d6f2a14be63b665b499d93a4392a
Chris Holcombe <xfactor973@gmail.com>
2016-07-11 14:58:00 -0700

    Fix the gh-pages script

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -7,7 +7,7 @@ cd target/doc
 git init
 git config user.name "Travis CI"
 git config user.email "cholcombe973@users.noreply.github.com"
-echo "<meta http-equiv=refresh content=0;url=qemu-rust/index.html>" > index.html
+echo "<meta http-equiv=refresh content=0;url=qemu_rust/index.html>" > index.html
 git add .
 git commit --quiet -m "Deploy to GH pages."
 git push --force --quiet "https://${GH_TOKEN}@github.com/cholcombe973/qemu-rust.git" master:gh-pages &> /dev/null
